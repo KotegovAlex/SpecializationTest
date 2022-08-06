286b563a767b373b6ec3f7250bc822ab6c4c9352
Kotegov_Alexander <s.kotegov2010@yandex.ru>
2022-08-06 16:56:30 +0500

    Added README file

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Ввод строки пользователем
+string EnterData(string message)
+{
+    Console.WriteLine(message);
+    string input = Console.ReadLine();
+    return input;
+}
+
+// Преобразование строки в массив строк
+string[] StringArray(string InputString)
+{
+    string[] ResultString = InputString.Split(' ');
+    return ResultString;
+}
+
+// Формирование массива строк с длиной меньше ллибо равной трём символам
+string[] SpecLengthElementsArray(string[] InputArray, int ElementLength)
+{
+    string[] ResultArray = new string[1];
+    int j = 0;
+    int n = InputArray.Length;
+    for (int i = 0; i < n; i++)
+    {
+        if (InputArray[i].Length <= ElementLength)
+        {
+            ResultArray[j] = InputArray[i];
+            Array.Resize(ref ResultArray, ResultArray.Length + 1);
+            j++;
+        }
+        else continue;
+    }
+    return ResultArray;
+}
+
+string result = EnterData("Введите массив строк: ");
+string[] resultString = StringArray(SpecLengthElementsArray(result));
+
+Console.WriteLine("Итоговый массив: [" + String.Join(", ", resultString) + "]");
